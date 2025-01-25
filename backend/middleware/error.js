04be673d4effca3e77006d3a4dd09d38726637ef
tanishq45632 <tanishqsinghal483@gmail.com>
2025-01-25 10:28:58 +0530

    added

---
+++ backend/middleware/error.js
@@ -0,0 +1,35 @@
+const ErrorHandler = require("../utils/ErrorHandler");
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error";
+
+  // Wrong MongoDB Id error
+  if (err.name === "CastError") {
+    const message = `Resources not found with this id. Invalid ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Duplicate key error
+  if (err.code === 11000) {
+    const message = `Duplicate key ${Object.keys(err.keyValue)} Entered`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Wrong JWT error
+  if (err.name === "JsonWebTokenError") {
+    const message = `Your URL is invalid, please try again letter`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // JWT expired
+  if (err.name === "TokenExpiredError") {
+    const message = `Your Url is expired please try again letter!`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+};
